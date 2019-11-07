9157e1c52f82acbe295565d18bccc325c277a5cb
Anastasia Karavdina <anastasia.karavdina@desy.de>
2019-11-07 15:29:49 +0100

    add example of conversion into Jagged Array

---
+++ InputsPreparation/UHH2toJaggedArray/steer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+from ConvertRoot import *
+
+for M in '700', '800', '900', '1000', '1100','1200','1300','1500':
+    ConvertRootToArray(name='MC_TstarTstarToTgluonTgluon_M-'+M+'_Run2016v3')
+#    ConvertRootToArray(name='MC_TstarTstarToTgammaTgamma_M-'+M+'_Run2016v3')
+
